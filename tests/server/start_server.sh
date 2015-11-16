35b3911cc5aafc92e28a1cfca0e7d1a1bc5f523e
freek <freek@spatie.be>
2015-11-16 17:39:32 +0100

    first stab at getting travis to work with express server

---
+++ tests/server/start_server.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+npm install
+node server.js
+
+if [ -z ${TRAVIS_JOB_ID} ]; then
+    # not running under travis, stay in foreground until stopped
+    node server.js
+else
+    # running under travis, daemonize
+    (node server.js &) || /bin/true
+fi
