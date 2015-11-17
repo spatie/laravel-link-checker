2c6a461c729f9b0918c9e9bf70af02e937b64bcb
Freek Van der Herten <freek@spatie.be>
2015-11-17 15:20:11 +0100

    Update server.js

--- tests/server/server.js
+++ tests/server/server.js
@@ -7,16 +7,16 @@ app.get('/', (req, res) => {
         .map((statusCode) => `<a href="${statusCode}">${statusCode}</a><br />`)
         .join('')
 
-    console.log(`Visit on /`);
+    console.log(`Visit on /`)
 
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end(html);
+    res.writeHead(200, {'Content-Type': 'text/html'})
+    res.end(html)
 });
 
 app.get('/:responseCode', (req, res) => {
-    let responseCode = req.params.responseCode;
+    let responseCode = req.params.responseCode
 
-    console.log(`Visit on ${responseCode}`);
+    console.log(`Visit on ${responseCode}`)
 
     res.status(responseCode).send(`Responsecode ${responseCode}`)
 });
@@ -25,5 +25,5 @@ var server = app.listen(4020,  () => {
     let host = server.address().address
     let port = server.address().port
 
-    console.log('Testing server listening at http://%s:%s', host, port);
+    console.log('Testing server listening at http://%s:%s', host, port)
 });
