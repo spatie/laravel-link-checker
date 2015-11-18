e0c47fb3b9c3370bddd30c19b938572378bc45f1
freek <freek@spatie.be>
2015-11-18 18:30:14 +0100

    fix tests

--- tests/server/start_server.sh
+++ tests/server/start_server.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 
-npm install
-node server.js
+
+
 
 if [ -z ${TRAVIS_JOB_ID} ]; then
     # not running under travis, stay in foreground until stopped
     node server.js
 else
+    cd tests/server
+    
+    npm install
     # running under travis, daemonize
     (node server.js &) || /bin/true
 fi
