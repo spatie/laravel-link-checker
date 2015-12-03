172d4183b0f65ca18357e63b5c36c4b2fdd028fc
Freek Van der Herten <freek@spatie.be>
2015-12-03 17:27:31 +0100

    Update crawlReport.blade.php

--- resources/views/crawlReport.blade.php
+++ resources/views/crawlReport.blade.php
@@ -1,6 +1,6 @@
 <h2>Found broken urls</h2>
 @foreach($urlsGroupedByStatusCode as $statusCode => $urls)
     @if (! starts_with($statusCode, [2,3]))
-        Crawled {{ count($urls) }} with status code {{ $statusCode }}<br />
+        Crawled {{ count($urls) }} links with status code {{ $statusCode }}<br />
     @endif
 @endforeach
