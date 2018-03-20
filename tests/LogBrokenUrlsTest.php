501c4f8f642ec9c05436ea3a25adcdb839e4382b
Brent Roose <brent@spatie.be>
2018-03-20 10:14:06 +0100

    Update spatie/crawler to 4.0

--- tests/LogBrokenUrlsTest.php
+++ tests/LogBrokenUrlsTest.php
@@ -15,8 +15,8 @@ class LogBrokenUrlsTest extends IntegrationTest
 
         $this->app[Kernel::class]->call('link-checker:run', ['--url' => $this->appUrl]);
 
-        $this->assertLogContainsTextAfterLastMarker('400 Bad Request - http://localhost:4020/400');
-        $this->assertLogContainsTextAfterLastMarker('500 Internal Server Error - http://localhost:4020/500');
+        $this->assertLogContainsTextAfterLastMarker('GET http://localhost:4020/400` resulted in a `400 Bad Request`');
+        $this->assertLogContainsTextAfterLastMarker('GET http://localhost:4020/500` resulted in a `500 Internal Server Error`');
         $this->assertLogContainsTextAfterLastMarker('link checker summary');
         $this->assertLogContainsTextAfterLastMarker('Crawled 1 url(s) with statuscode 400');
         $this->assertLogContainsTextAfterLastMarker('Crawled 1 url(s) with statuscode 500');
@@ -32,8 +32,8 @@ class LogBrokenUrlsTest extends IntegrationTest
         $this->app['config']->set('laravel-link-checker.reporters.exclude_status_codes', [500]);
         $this->app[Kernel::class]->call('link-checker:run', ['--url' => $this->appUrl]);
 
-        $this->assertLogContainsTextAfterLastMarker('400 Bad Request - http://localhost:4020/400');
-        $this->assertLogNotContainsTextAfterLastMarker('500 Internal Server Error - http://localhost:4020/500');
+        $this->assertLogContainsTextAfterLastMarker('GET http://localhost:4020/400` resulted in a `400 Bad Request`');
+        $this->assertLogNotContainsTextAfterLastMarker('GET http://localhost:4020/500` resulted in a `500 Internal Server Error`');
         $this->assertLogContainsTextAfterLastMarker('link checker summary');
         $this->assertLogContainsTextAfterLastMarker('Crawled 1 url(s) with statuscode 400');
         $this->assertLogNotContainsTextAfterLastMarker('Crawled 1 url(s) with statuscode 500');
